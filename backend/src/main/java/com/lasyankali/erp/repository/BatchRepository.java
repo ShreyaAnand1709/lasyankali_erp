28dda1e1c6c6906237e2d8e51517681027d97e3a
Shreya Anand Andewadikar <shreya1709202@gmail.com>
2026-09-02 01:33:38 +0530

    batch repo added

--- backend/src/main/java/com/lasyankali/erp/repository/BatchRepository.java
+++ backend/src/main/java/com/lasyankali/erp/repository/BatchRepository.java
@@ -1,11 +1,28 @@
 package com.lasyankali.erp.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.lasyankali.erp.entity.Batch;
+import com.lasyankali.erp.entity.Student;
+import com.lasyankali.erp.entity.enums.BatchStatus;
 
 public interface BatchRepository extends JpaRepository<Batch, Long>{
 	Optional<Batch> findByBatchCode(String batchCode);
+	boolean existsByBatchCode(String batchCode);
+	List<Batch> findByStatus(BatchStatus status);
+	@Query("""
+			SELECT b 
+			from Batch b
+			where b.status = com.lasyankali.erp.entity.enums.BatchStatus.ACTIVE
+			AND (
+			LOWER(b.batchCode) LIKE LOWER(CONCAT('%', :keyword, '%'))
+	         OR LOWER(b.batchName) LIKE LOWER(CONCAT('%', :keyword, '%'))
+	         )
+			""")
+	List<Batch> searchBatch(@Param("keyword") String keyword);
 }
